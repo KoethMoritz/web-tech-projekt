e3cc686d9f5c1fd3065fc9d6bffa958f84b4cc4c
KoethMoritz <moritzkoeth@gmail.com>
2023-11-03 00:50:29 +0100

    Thing & ThingService update

--- webtech/src/main/java/htwberlin/webtech/Thing.java
+++ webtech/src/main/java/htwberlin/webtech/Thing.java
@@ -34,4 +34,31 @@ public class Thing {
     public Long getId() {
         return id;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Thing)) return false;
+
+        Thing thing = (Thing) o;
+
+        if (getId() != null ? !getId().equals(thing.getId()) : thing.getId() != null) return false;
+        return getName() != null ? getName().equals(thing.getName()) : thing.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Thing{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
 }
--- webtech/src/main/java/htwberlin/webtech/ThingService.java
+++ webtech/src/main/java/htwberlin/webtech/ThingService.java
@@ -3,6 +3,9 @@ package htwberlin.webtech;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ThingService {
 
@@ -15,6 +18,13 @@ public class ThingService {
 
     public Thing get(Long id) {
         return repo.findById(id).orElseThrow(() -> new RuntimeException());
+    }
+
+    public List<Thing> getAll() {
+        Iterable<Thing> iterator = repo.findAll();
+        List<Thing> things = new ArrayList<>();
+        for (Thing thing : iterator)  things.add(thing);
+        return things;
     }
 
 }
