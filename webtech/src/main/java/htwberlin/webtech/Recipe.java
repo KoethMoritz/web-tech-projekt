d873f52fd9aae62bf375241e11bcbf7b3ca4f5f5
KoethMoritz <moritzkoeth@gmail.com>
2023-11-17 13:40:05 +0100

    kleine Änderung

--- webtech/src/main/java/htwberlin/webtech/Recipe.java
+++ webtech/src/main/java/htwberlin/webtech/Recipe.java
@@ -55,7 +55,7 @@ public class Recipe {
 
     @Override
     public String toString() {
-        return "Thing{" +
+        return "Recipe{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
